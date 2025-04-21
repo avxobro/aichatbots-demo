bdfa4187dd83ae0c356bb77f944adc62dc1c54ce
Allan Bradbury <allancbradbury@gmail.com>
2025-04-21 15:05:15 +0100

    clean up css

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
-  // Remove the custom webpack config as it might interfere with default CSS handling
-  /* webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  }, */
+  output: 'standalone'
 }
 
 module.exports = nextConfig
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   plugins: {
     'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true
-      }
-    }
+    'autoprefixer': {}
   }
 }
+
